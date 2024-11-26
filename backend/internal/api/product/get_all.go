9f2303d38cc8d505e68969aa8816926013bc2b70
fi44er <fi44er@fi44er.fi44er>
2024-11-26 19:44:36 +0500

    small fix

--- backend/internal/api/product/get_all.go
+++ backend/internal/api/product/get_all.go
@@ -1,6 +1,9 @@
 package product
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Fi44er/sdmedik/backend/pkg/errors"
+	"github.com/gofiber/fiber/v2"
+)
 
 func (i *Implementation) GetAll(ctx *fiber.Ctx) error {
 	offset := ctx.QueryInt("offset")
@@ -8,7 +11,8 @@ func (i *Implementation) GetAll(ctx *fiber.Ctx) error {
 
 	products, err := i.productService.GetAll(ctx.Context(), offset, limit)
 	if err != nil {
-		return ctx.Status(400).JSON(err)
+		code, msg := errors.GetErroField(err)
+		return ctx.Status(code).JSON(msg)
 	}
 
 	return ctx.Status(200).JSON(products)
