7a954ccefbaa9688c175e10f1d094413ea8423a3
valey88 <valej200554@gmail.com>
2024-12-05 09:57:09 +0300

    ads

--- frontend/src/pages/account/UserAccount.jsx
+++ frontend/src/pages/account/UserAccount.jsx
@@ -1,6 +1,33 @@
-import { Box, Typography, CircularProgress } from "@mui/material";
+import {
+  Box,
+  Typography,
+  CircularProgress,
+  Container,
+  Card,
+  CardMedia,
+  CardContent,
+  CardHeader,
+  IconButton,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid2";
 import useUserStore from "../../store/userStore";
+const Product = [
+  {
+    id: 1,
+    title: `Инвалидная коляскаTrend 40`,
+    country: `Бельгия`,
+    price: `25000,00  ₽ `,
+    image: `/public/wheelchair.png`,
+  },
+  {
+    id: 2,
+    title: `Инвалидная коляскаTrend 40`,
+    country: `Бельгия`,
+    price: `25000,00  ₽ `,
+    image: `/public/wheelchair.png`,
+  },
+];
 
 export default function UserAccount() {
   const { getUserInfo, user } = useUserStore();
@@ -32,15 +59,159 @@ export default function UserAccount() {
   // Если данные загружены, отображаем их
   return (
     <Box>
-      {user && user.data ? ( // Проверяем, есть ли данные
-        <>
-          <Typography>Email: {user.data.email}</Typography>
-          <Typography>ФИО: {user.data.fio}</Typography>
-          <Typography>Телефон: {user.data.phone_number}</Typography>
-        </>
-      ) : (
-        <Typography>No user data available</Typography>
-      )}
+      <Container>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gridGap: 40,
+            flexDirection: { xs: "column", md: "unset" },
+          }}
+        >
+          <Box>
+            <img src="/user_Profile.png" alt="" />
+          </Box>
+          {user && user.data ? ( // Проверяем, есть ли данные
+            <Box>
+              <Typography variant="h5">{user.data.fio}</Typography>
+              <Typography variant="h6">{user.data.email}</Typography>
+              <Typography variant="h6">{user.data.phone_number}</Typography>
+              
+            </Box>
+          ) : (
+            <Typography>No user data available</Typography>
+          )}
+        </Box>
+        <Box>
+          <Typography>Заказы</Typography>
+
+          <Box>
+            <Grid
+              container
+              spacing={{ xs: 2, md: 5 }}
+              columns={{ xs: 4, sm: 4, md: 4 }}
+              sx={{ mt: 4 }}
+            >
+              {Product.map((e) => {
+                return (
+                  <Grid
+                    item
+                    key={e.id}
+                    xs={1}
+                    sm={1}
+                    md={1}
+                    sx={{
+                      width: "100%",
+                    }}
+                  >
+                    <Card
+                      sx={{
+                        width: { xs: "100%", lg: "100%" },
+                        background: "#F5FCFF",
+                        display: "flex",
+                        flexDirection: { xs: "column", md: "unset" },
+                        p: { xs: 0, md: 3 },
+                      }}
+                    >
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "center",
+                          pt: 3,
+                          pl: 3,
+                        }}
+                      >
+                        <CardMedia
+                          component="img"
+                          image={e.image}
+                          alt={"wheelchair"}
+                          sx={{
+                            width: { xs: "125px", md: "200px" },
+                            height: { xs: "125px", sm: "200px", md: "200px" },
+                            objectFit: "cover",
+                          }}
+                        />
+                      </Box>
+                      <CardContent
+                        sx={{
+                          display: "flex",
+                          flexDirection: { xs: "column", md: "unset" },
+                        }}
+                      >
+                        <Box>
+                          <CardHeader
+                            title={e.title}
+                            sx={{ p: { xs: 0, md: 2 } }}
+                          />
+                          <Typography
+                            variant="body2"
+                            color="text.secondary"
+                            sx={{ ml: { xs: 0, md: 2 } }}
+                          >
+                            {e.country}
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            sx={{ color: "black", ml: { xs: 0, md: 2 } }}
+                          >
+                            {e.price}
+                          </Typography>
+                        </Box>
+                      </CardContent>
+                      <CardContent
+                        sx={{
+                          display: "flex",
+                          flexDirection: { xs: "column", md: "column" },
+                        }}
+                      >
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <Typography
+                            variant="h5"
+                            sx={{ color: "black", mt: { xs: 0, md: 2 } }}
+                          >
+                            Статус заказа
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            component="p"
+                            sx={{ color: "black", mt: { xs: 0, md: 2 } }}
+                          >
+                            Арсений сделай нормальную карточку
+                          </Typography>
+                        </Box>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                          }}
+                        >
+                          <Typography
+                            variant="h5"
+                            sx={{ color: "black", mt: { xs: 0, md: 2 } }}
+                          >
+                            Адрес
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            sx={{ color: "black", mt: { xs: 0, md: 2 } }}
+                          >
+                            Арсений сделай нормальную карточку
+                          </Typography>
+                        </Box>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
     </Box>
   );
 }
