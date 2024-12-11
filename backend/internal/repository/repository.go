896780004bb6792edceeb2ed792998c1d924dfa9
fi44er <fi44er@fi44er.fi44er>
2024-12-11 15:16:52 +0500

    add transaction in create product method

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -26,6 +26,7 @@ type IProductRepository interface {
 	Create(ctx context.Context, data *model.Product, tx *gorm.DB) error
 	GetAll(ctx context.Context, offset int, limit int) ([]model.Product, error)
 	GetByID(ctx context.Context, id string) (model.Product, error)
+	GetByArticle(ctx context.Context, article string) (model.Product, error)
 	Delete(ctx context.Context, id string) error
 	Update(ctx context.Context, data *model.Product) error
 }
