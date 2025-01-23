0e130d9121f2a90834d9e3bcce3e0031d406bbfe
Fi44er <vaceslavmalugin81@gmail.com>
2025-01-23 23:10:22 +0500

    change response top products func

--- backend/internal/service/product/get_top_products.go
+++ backend/internal/service/product/get_top_products.go
@@ -6,6 +6,38 @@ import (
 	"github.com/Fi44er/sdmedik/backend/internal/response"
 )
 
-func (s *service) GetTopProducts(ctx context.Context, limit int) ([]response.ProductPopularity, error) {
-	return s.repo.GetTopProducts(ctx, limit)
+func (s *service) GetTopProducts(ctx context.Context, limit int) (*[]response.TopProductRes, error) {
+	top, err := s.repo.GetTopProducts(ctx, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	productsIds := []string{}
+
+	for _, item := range top {
+		productsIds = append(productsIds, item.ProductID)
+	}
+
+	products, err := s.repo.GetByIDs(ctx, productsIds)
+	if err != nil {
+		return nil, err
+	}
+
+	topProductList := []response.TopProductRes{}
+	for index, product := range *products {
+		var imageUrl string
+		if len(product.Images) > 0 {
+			imageUrl = product.Images[0].Name
+		}
+		topProductList = append(topProductList, response.TopProductRes{
+			ID:         product.ID,
+			Price:      product.Price,
+			OrderCount: top[index].OrderCount,
+			Article:    product.Article,
+			Image:      imageUrl,
+			Name:       product.Name,
+		})
+	}
+
+	return &topProductList, nil
 }
