93b33b1d9bb715ccdcd945c1587a6f6d990c370b
Fi44er <vaceslavmalugin81@gmail.com>
2025-03-07 13:34:46 +0500

    [upd]: fix add item to guest basket; fix deleting item from guest basket

--- backend/internal/service/basket/delete_item.go
+++ backend/internal/service/basket/delete_item.go
@@ -2,13 +2,15 @@ package basket
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/Fi44er/sdmedik/backend/internal/model"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
 
 func (s *service) DeleteItem(ctx context.Context, itemID string, userID string, sess *session.Session) error {
-	if userID == "" {
+	if userID != "" {
 		basket, err := s.GetByUserID(ctx, userID, sess)
 		if err != nil {
 			return err
@@ -18,17 +20,31 @@ func (s *service) DeleteItem(ctx context.Context, itemID string, userID string, 
 			return err
 		}
 	} else {
-		basket := sess.Get("basket").(*model.Basket)
+		var basket model.Basket
+		str, ok := sess.Get("basket").(string)
+		if !ok {
+			return fmt.Errorf("Ошибка: interface{} не является строкой")
+		}
+		if err := json.Unmarshal([]byte(str), &basket); err != nil {
+			return err
+		}
+
 		newBasket := []model.BasketItem{}
 		for _, item := range basket.Items {
-			if item.ID != itemID {
+			if item.ProductID != itemID {
 				newBasket = append(newBasket, item)
 				break
 			}
 		}
 
 		basket.Items = newBasket
-		sess.Set("basket", basket)
+
+		basketStr, err := json.Marshal(basket)
+		if err != nil {
+			return fmt.Errorf("failed to marshal basket: %w", err)
+		}
+
+		sess.Set("basket", string(basketStr))
 		if err := sess.Save(); err != nil {
 			return err
 		}
