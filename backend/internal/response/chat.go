196d49dfa4be525ef98f193fb29b433de0abb190
fi44er <fi44er@fi44er.fi44er>
2025-06-16 13:54:37 +0500

    add mark as read msg

--- backend/internal/response/chat.go
+++ backend/internal/response/chat.go
@@ -3,7 +3,7 @@ package response
 import "github.com/Fi44er/sdmedik/backend/internal/model"
 
 type ChatResponse struct {
-	ID          string `gorm:"primaryKey;type:string;" json:"id"`
-	Messages    []model.Message
-	UnreadCount int64
+	ID          string          `json:"id"`
+	Messages    []model.Message `json:"messages"`
+	UnreadCount int64           `json:"unread_count"`
 }
