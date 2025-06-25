18747dec027b440c2ee146ae2c9f3538e70dcc92
Fi44er <vaceslavmalugin81@gmail.com>
2025-06-25 15:56:15 +0500

    *

--- backend/internal/service/auth/login.go
+++ backend/internal/service/auth/login.go
@@ -56,5 +56,9 @@ func (s *service) Login(ctx context.Context, data *dto.Login, userAgent string, 
 		return "", "", err
 	}
 
+	if err := session.Destroy(); err != nil {
+		s.logger.Errorf("Error during destroying session: %s", err.Error())
+	}
+
 	return *accessTokenDetails.Token, *refreshTokenDetails.Token, nil
 }
