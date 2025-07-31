63b42dcbe71b9ce36d2d8f79370a2e739e6590b7
Fi44er <vaceslavmalugin81@gmail.com>
2025-07-31 12:59:14 +0500

    *

--- backend/internal/repository/product/repository.go
+++ backend/internal/repository/product/repository.go
@@ -92,7 +92,10 @@ func (r *repository) Update(ctx context.Context, data *model.Product, tx *gorm.D
 		}
 	}
 
-	result := db.WithContext(ctx).Model(data).Select("*").Updates(data)
+	result := db.WithContext(ctx).Model(data).Updates(data).UpdateColumns(map[string]interface{}{
+		"Prewiu":    data.Preview,
+		"Nameplate": data.Nameplate,
+	})
 	if err := result.Error; err != nil {
 		r.logger.Errorf("Failed to update product: %v", err)
 		return err
